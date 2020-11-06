80548db5b600bc80c212eac5297587885595329d
ploymuddle <bunprasoet227@gmail.com>
2020-11-06 16:57:37 +0700

    'login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $userData = [
+            [
+               'name'=>'Admin',
+               'username'=>'admin',
+               'email'=>'admin@hotmail.com',
+                'is_admin'=>'0',
+               'password'=> bcrypt('1234'),
+            ],
+            [
+               'name'=>'Normal User',
+               'username'=>'admin',
+               'email'=>'user@gmail.com',
+               'is_admin'=>'1',
+               'password'=> bcrypt('1234'),
+            ],
+        ];
+  
+        foreach ($userData as $key => $val) {
+            User::create($val);
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('admin/home', [App\Http\Controllers\HomeController::class, 'handleAdmin'])->name('admin.route')->middleware('admin');
