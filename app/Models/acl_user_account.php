598ed0f4569b7363fbf803f00feface05597eb29
sutheemon <sutheemon@bitbucket.com>
2020-11-09 01:06:50 +0700

    create model

---
+++ app/Models/acl_user_account.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class acl_user_account extends Model
+{
+    use HasFactory;
+}
